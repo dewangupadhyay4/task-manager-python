edc5d3421d2ef04dc051de11f7371dbd8800fe82
dewangupadhyay4 <dewangupadhyay23@gmail.com>
2025-07-02 06:54:58 +0530

    this is new commit

--- taskmanagerapp/serializers.py
+++ taskmanagerapp/serializers.py
@@ -12,6 +12,7 @@ class RegisterSerializer(serializers.ModelSerializer):
     class Meta:
         model=User
         fields=['username','email','password']
+        extra_kwargs={'password': {'write_only': True}}
 
     def create(self, validated_data):
         user=User.objects.create_user(
@@ -19,5 +20,7 @@ class RegisterSerializer(serializers.ModelSerializer):
             email=validated_data.get('email'),
             password=validated_data['password=']
         )
+        user.set_password(validated_data['password'])
+        user.save()
 
         return user
--- taskmanagerapp/urls.py
+++ taskmanagerapp/urls.py
@@ -5,7 +5,7 @@ from .views import TaskListCreateView, TaskRetrieveUpdateDelete, CustomAuthToken
 urlpatterns = [
     path('tasks/', TaskListCreateView.as_view(), name="task_list_create"),
     path('tasks/<int:pk>/', TaskRetrieveUpdateDelete.as_view(), name="ret_update_delete"),
-    path('register/', register_user, name='register_user')
+    path('register/', register_user, name=)
 ]
 
 urlpatterns += [
